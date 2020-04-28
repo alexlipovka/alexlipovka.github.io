a83242fcea891c1b21ef01492c3ea01076dd66e7
alexlipovka <alex.lipovka@gmail.com>
2020-04-28 19:44:15 +0200

    12

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,22 +1,17 @@
-let capture;
-
 function setup() {
   // put setup code here
   createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
-  capture.size(640, 480);
 }
 
 function draw() { 
 	background(255);
   // put drawing code here
 
-  image(capture, 0, 0);
-  filter(INVERT);
   ellipseMode(CENTER);
   ellipse(mouseX, mouseY, 200, 200);
 }
 
 function windowResized() {
+  
 	resizeCanvas(windowWidth, windowHeight);
 }
