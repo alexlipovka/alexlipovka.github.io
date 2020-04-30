f5106415105b2514b50b1c5d5c6b29e87c9babd5
alexlipovka <alex.lipovka@gmail.com>
2020-04-30 18:04:10 +0200

    sketch 03 - staged folder

---
+++ p5/sketch_03/sketch.js
@@ -0,0 +1,42 @@
+// Настройка области рисования
+
+// Настраиваем область рисования. Сразу же сделаем заливку белым цветом.
+
+
+function setup() {
+    //стандартное создание области рисования размеров с текущее окно
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+}
+
+// Рисуем квадратики
+
+// Рисование будет только при нажатой левой клавише мыши.
+
+
+function draw() {
+    fill(100);
+    textSize(16);
+    text('ЛКМ — рисование\nПРОБЕЛ — сброс', 10, 10, 300, 100);
+    noStroke(); //отключаем отрисовку обводок
+    fill(160);
+    rectMode(CORNERS);
+    if(mouseIsPressed) {
+        if(mouseButton === LEFT) {
+            rect(mouseX, mouseY, pmouseX, pmouseY);
+        }
+    }
+    if(keyIsPressed) {
+        if(key === ' ') {
+            background(255);
+        }
+    }
+}
+
+// Обработка изменения размера окна
+
+
+function windowResized() {
+    //в случае изменения окна надо перестроить область рисования
+    resizeCanvas(windowWidth, windowHeight);
+}
