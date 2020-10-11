53bb0602393905af2fc69abf7d97ba89ced02c64
alexlipovka <alex.lipovka@gmail.com>
2020-10-11 23:52:36 +0200

    немного зачистки

--- leaflet/map_01/map.js
+++ leaflet/map_01/map.js
@@ -1,17 +1,9 @@
 // Веб-карта
 
+// Зачастую код для создания и подключения карты внедряют в блок =script= в тело файла =index.html=, но в целом удобнее вынести код в отдельный файл =map.js=. Важно, чтобы ссылка на этот файл была после создания блока =div= с идентификатором карты.
+
 
 var mymap = L.map('mapid').setView([56.03, 92.9], 12);
-// mymap.options.crs = L.CRS.EPSG3395;
-
-// L.tileLayer('http://vec01.maps.yandex.net/tiles?l=map&x={x}&y={y}&z={z}', {
-//     opacity: 1.0,
-//     attribution: '',
-//     minZoom: 1,
-//     maxZoom: 28,
-//     minNativeZoom: 0,
-//     maxNativeZoom: 18
-// }).addTo(mymap);  
 
 L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     opacity: 1.0,
